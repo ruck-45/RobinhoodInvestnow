a4cd1887e55e58e5c75111e45fa31ee832310dd7
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-01-03 00:05:22 +0530

    popup makeover 02

--- src/components/Home/subComponents/Popover.tsx
+++ src/components/Home/subComponents/Popover.tsx
@@ -4,6 +4,7 @@ import { useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FaPhone } from "react-icons/fa6";
 import { BiSolidPhoneCall } from "react-icons/bi";
+import { MdAddIcCall } from "react-icons/md";
 
 // Local Files
 import { updatePopoverStatus } from "../../../store/popoverStatusSlice";
@@ -20,7 +21,7 @@ const Popover = () => {
       setTimeout(() => {
         PopoverButton.current.click();
         dispatch(updatePopoverStatus(true));
-      }, 2000);
+      }, 500);
     }
   }, []);
 
@@ -32,17 +33,24 @@ const Popover = () => {
       <Modal isOpen={isOpen} onOpenChange={onOpenChange} className="dark text-white sm:hidden">
         <a href="tel:+1-888-355-9278">
           <ModalContent className="h-[99%]">
-                <ModalHeader className="flex items-center gap-[1rem]">
-                  <Button isIconOnly variant="solid" radius="full" className="text-[1rem]">
-                    <FaPhone />
-                  </Button>
-                  <p>Contact Us</p>
-                </ModalHeader>
-                <ModalBody className="flex flex-col justify-center items-center">
-                  <BiSolidPhoneCall  className="text-[20rem]"/>
-                  <p className="text-[#f31260] font-bold">Call us now</p>
-                  <p className="text-center">to speak live with a person on our team. we're committed to be there for our customers.</p>
-                </ModalBody>
+            <ModalHeader className="flex items-center gap-[1rem]">
+              <Button isIconOnly variant="solid" radius="full" className="text-[1rem]">
+                <FaPhone />
+              </Button>
+              <p>Contact Us</p>
+            </ModalHeader>
+            <ModalBody className="flex flex-col justify-center items-center">
+              <h1 className="text-[3rem] leading-[2.2rem] font-bold font-['kalina'] text-center mb-[2rem]">
+                Robinhood Investment
+              </h1>
+              <p className="font-bold mb-[2rem] text-[1.5rem]">More Info</p>
+              <BiSolidPhoneCall className="text-[5rem] " />
+              <p className="text-[#f31260] font-bold text-[2rem]">Call us now</p>
+              <p className="text-center">
+                to speak live with a person on our team. we're committed to be there for our customers.
+              </p>
+            </ModalBody>
+            <MdAddIcCall className="text-[3rem] mx-auto bg-[#006FEE] w-full p-[0.5rem]" />
           </ModalContent>
         </a>
       </Modal>
